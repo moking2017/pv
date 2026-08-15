3232ff81aab76cea07a65f3ddd848fe8fa20a419
moking2017 <135400083+moking2017@users.noreply.github.com>
2026-08-15 21:24:11 +0800

    Update ppv_to_m3u.py

--- ppv_to_m3u.py
+++ ppv_to_m3u.py
@@ -13,7 +13,7 @@ from datetime import datetime, timezone, timedelta
 
 API_URL     = "https://api.ppv.st/api/streams"
 OUTPUT_FILE = "ppv_events.m3u"
-BASE_STREAM = "https://ppv2.168.us.kg/stream?uri={uri}"
+BASE_STREAM = "https://ppv2.168.us.kg/?id={uri}"
 TZ_OFFSET   = timedelta(hours=8)   # UTC → UTC+8
 
 HEADERS = {
